fe2e4627631dc9c77e96d176b76ca15c331c02fb
Emily Martinez <mle626@gmail.com>
2014-01-24 04:54:50 -0800

    Inital Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kittens::Application.config.secret_key_base = 'f554464821f8516cc63b9a69bdcd3f97504c6ebad1101a17d51c508a9ab18a8b1effb1470ef2ae1b6605c68a8afdd1d6a7f6399041f7855ee4b786dbe2882b2c'
