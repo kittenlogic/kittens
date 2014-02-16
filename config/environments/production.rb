0d5f509386819eb151d16491d47344b813d2bb0b
Emily Martinez <mle626@gmail.com>
2014-02-16 13:29:22 -0800

    Change S3 Host Config 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Kittens::Application.configure do
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
-    Paperclip::Attachment.default_options[:s3_host_name] = 'meows.s3-website-us-west-1.amazonaws.com'
+    :s3_host_name => 'meows.s3-website-us-west-1.amazonaws.com'
     
   }
 end
