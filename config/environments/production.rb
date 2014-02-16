d034414fc4f23107b7f83756a62011878c4c4cb8
Emily Martinez <mle626@gmail.com>
2014-02-16 13:44:15 -0800

    Change S3 Host Config 3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,12 +84,12 @@ Kittens::Application.configure do
   # Paperclip should use Amazon S3 on Heroku
   config.paperclip_defaults = {
     :storage => :s3,
+    :s3_host_name => 's3-us-west-1.amazonaws.com',
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
-    :s3_host_name => 'meows.s3-website-us-west-1.amazonaws.com'
     
   }
 end
