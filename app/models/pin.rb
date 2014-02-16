25e5c9ace73d41b6e754af317cf837724fe9d755
Emily Martinez <mle626@gmail.com>
2014-02-15 20:46:38 -0800

    Add Imagemagick styles to pin.rb

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,7 @@
 class Pin < ActiveRecord::Base
 
 	belongs_to :user
-	has_attached_file :image #, :styles => { :medium => "320x240>" }
+	has_attached_file :image, :styles => { :medium => "320x240>" }
 
 	validates_attachment :image, presence: true,
 								 :content_type => { :content_type =>  ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']}
